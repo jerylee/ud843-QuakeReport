849ae03888176081899533f40f238a8bf1bd87d4
Jery <mr.jerylee@gmail.com>
2018-03-09 15:36:46 +0700

    update

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -11,8 +11,7 @@ public class Earthquake {
      * Constructs a new {@link Earthquake} object.
      *
      * @param magnitude is the magnitude (size) of the earthquake
-     * @param location is the city location of the earthquake
-     *
+     * @param location is the city location of the earthquake     *
      * @param timeInMilliseconds is the time in milliseconds (from he Epoch) when the earthquake happened.
      */
 
