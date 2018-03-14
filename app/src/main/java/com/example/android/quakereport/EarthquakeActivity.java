b87d5ced68bf05686486367cb036d48de829c9bc
Jery <mr.jerylee@gmail.com>
2018-03-14 21:45:25 +0700

    update

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -19,10 +19,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -60,24 +62,34 @@ public class EarthquakeActivity extends AppCompatActivity {
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(adapter);
+        if (earthquakeListView != null) {
+            earthquakeListView.setAdapter(adapter);
+        }
 
-        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                // Find the current earthquake that was clicked on
-                Earthquake currentEarthquake = adapter.getItem(position);
+        if (earthquakeListView != null) {
+            earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                    // Find the current earthquake that was clicked on
+                    Earthquake currentEarthquake = adapter.getItem(position);
 
-                // Convert the String URL into a URI object (to pass into the Intent constructor)
-                Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+                    // Convert the String URL into a URI object (to pass into the Intent constructor)
+                    Uri earthquakeUri = null;
+                    if (currentEarthquake != null) {
+                        earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Sorry, No Website Link", Toast.LENGTH_SHORT).show();
+                    }
 
-                // Create a new intent to view the earthquake URI
-                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+                    // Create a new intent to view the earthquake URI
+                    Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+                    // Send the intent to launch a new activity
+                    startActivity(websiteIntent);
 
-                // Send the intent to launch a new activity
-                startActivity(websiteIntent);
-            }
-        });
+
+                }
+            });
+        }
 
     }
 }
